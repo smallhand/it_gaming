c293b7499c7d872f9b7f4d19fe3031d1078e5c1a
smallhand <pc00083@gmail.com>
2022-09-18 23:33:37 +0800

    reduce array number

--- game_1a2b/game_1a2b.js
+++ game_1a2b/game_1a2b.js
@@ -5,62 +5,71 @@ let hint = document.getElementById('hint');
 hint.addEventListener('click', show_tar);
 
 const tar_arr = [];
-const guess_arr = [];
 
+let digits= 4;
 let round = 0;
 let tar = "";
-init();
+init()
 
 function init()
 {
     round = 0;
-    tar = "";
-    for (let i = 0; i < 4; i++)
+    for (let i = 0; i<10; i++)
     {
-        tar += Math.floor(Math.random() * 10);
+        tar_arr[i] = 0;
     }
 
+    tar = "";
+    for (let i = 0; i < digits; i++)
+    {
+        tar = Math.floor(Math.random() * 10);
+    }
 }
 
 function judge(guess_n)
 {
     let ans = "";
+    for (let i = 0; i<10; i++)
+    {
+        tar_arr[i] = 0;
+    }
+
     round += 1;
     if (guess_n === tar)
     {
-        alert("bingo!!! the target number is: " + tar + ", total guess " + round + " times");
+        alert("Bingo!!! the target number is: " + tar + ", total guess " + round + " times");
         location.reload();
         init();
     }
     else
     {
-        let A = 0;
-        for (let i = 0; i<10; i++)
+        for (let i = 0; i < digits; i++)
         {
-            tar_arr[i] = 0;
-            guess_arr[i] = 0;
+            tar_arr[Number(tar[i])] += 1;
         }
-        //====
-        for (let i = 0; i<4; i++)
+
+        let A = 0;
+        for (let i = 0; i<digits; i++)
         {
             if (guess_n[i] === tar[i])
             {
                 A += 1;
-            }
-            else
-            {
-                guess_arr[Number(guess_n[i])] += 1;
-                tar_arr[Number(tar[i])] += 1;
+                tar_arr[Number(tar[i])] -= 1;
             }
         }
-        let B = 0
-        for (let i = 0; i<10; i++)
+        let B = digits-A;
+        for (let i = 0; i<digits; i++)
         {
-            while (guess_arr[i] > 0 && tar_arr[i] > 0)
+            if (guess_n[i] !== tar[i])
             {
-                B += 1;
-                guess_arr[i] -= 1;
-                tar_arr[i] -= 1;
+                if (tar_arr[Number(guess_n[i])] === 0)
+                {
+                    B -= 1;
+                }
+                if (tar_arr[Number(guess_n[i])] > 0)
+                {
+                    tar_arr[Number(guess_n[i])] -= 1;
+                }
             }
         }
         ans = A + "A" + B + "B<br>";
