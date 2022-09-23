d2158715b4419de6b70c37e5960a3433037168de
smallhand <pc00083@gmail.com>
2022-09-24 00:03:19 +0800

    setInterval to auto move

--- game_snake/snake_main.js
+++ game_snake/snake_main.js
@@ -3,17 +3,13 @@ let bg_cvs = document.getElementById("bg_canvas");
 let ctx = bg_cvs.getContext("2d");
 const max_len   = 600;
 const max_width = 360;
-//ctx.moveTo(0, 0);
-//ctx.lineTo(0, max_width);
-//ctx.lineTo(max_len, max_width);
-//ctx.lineTo(max_len, 0);
-//ctx.lineTo(0, 0);
-//ctx.strokeRect(0, 0, max_len, max_width);
 ctx.stroke();
 
+let score_area = document.querySelector('.score_area');
 
 // snake attribute
 const snake = [];
+let score = 0;
 let snake_len = 3; // start len of snake
 let snake_sz = 30; // 蛇能走的步數，長邊: 600/30 = 20, 短邊: 300/30 = 10; also radius = 15
 let snake_r = 15; // 30 /2 
@@ -36,6 +32,9 @@ k_map.set("ArrowUp", 3);
 
 let body = document.body;
 body.addEventListener('keydown', getKey, false);
+
+// time interval
+let time_id;
 
 function getKey(e)
 {
@@ -67,7 +66,7 @@ function getKey(e)
         return;
     }
     // 2. meet body?
-    if (chk_meet_sth(tmp_x, tmp_y) === true)
+    if (chk_meet_body(tmp_x, tmp_y) === true)
     {
         alert("game over");
         location.reload();
@@ -77,10 +76,11 @@ function getKey(e)
     if (cur_direct === k_value)
     {
         // 3. eat food?
-        if (chk_meet_sth(food_x, food_y) === true)
+        if (chk_eat_food(food_x, food_y) === true)
         {
             snake.push({x: tmp_x, y: tmp_y});
             ctx.fillRect(tmp_x, tmp_y, snake_sz, snake_sz);
+
             ctx.clearRect(snake[0].x, snake[0].y, snake_sz, snake_sz);
             add_food();
             return;
@@ -106,7 +106,7 @@ function getKey(e)
         tmp_x += direct[k_value][0] * snake_sz;
         tmp_y += direct[k_value][1] * snake_sz;
 
-        if (chk_meet_sth(food_x, food_y) === true)
+        if (chk_eat_food(food_x, food_y) === true)
         {
             snake.push({x: tmp_x, y: tmp_y});
             ctx.fillRect(tmp_x, tmp_y, snake_sz, snake_sz);
@@ -130,40 +130,48 @@ function getKey(e)
         snake.shift();
         cur_direct = k_value;
     }
+}
+function auto_move()
+{
+    let tmp_x, tmp_y;
+    tmp_x = snake[snake.length-1].x + direct[cur_direct][0] * snake_sz;
+    tmp_y = snake[snake.length-1].y + direct[cur_direct][1] * snake_sz;
 
-
-    /*
-    switch(e.code)
+    if ((tmp_x >= max_len) || (tmp_x < 0) || (tmp_y >= max_width) || (tmp_y<0))
     {
-        case 'ArrowRight': // cur direct[2] 會影響
-        {
-            if (cur_direct === 2)
-            {
-                cur_direct = 0;
-            }
-            break;
-        }
-        case 'ArrowDown':
-        {
-            break;
-        }
-        case 'ArrowLeft':
-        {
-            break;
-        }
-        case 'ArrowUp':
-        {
-            break;
-        }
-        default:
-        {
-            break;
-        }
+        alert("game over");
+        clearInterval(time_id);
+        location.reload();
     }
-    */
+    else if (chk_meet_body(tmp_x, tmp_y) == true)
+    {
+        alert("game over");
+        clearInterval(time_id);
+        location.reload();
+    }
+
+    else if (chk_eat_food(food_x, food_y) == true)
+    {
+        snake.push({x: tmp_x, y: tmp_y});
+        ctx.fillRect(tmp_x, tmp_y, snake_sz, snake_sz);
+        add_food();
+    }
+    else
+    {
+        //console.log(tmp_x);
+        snake.push({x:tmp_x, y:tmp_y});
+        ctx.fillRect(tmp_x, tmp_y, snake_sz, snake_sz);
+
+        //ctx.fillStyle = c_body;
+        //ctx.fillRect(snake[snake.length-2].x, snake[snake.length-2].y, snake_sz, snake_sz);
+        
+        ctx.clearRect(snake[0].x, snake[0].y, snake_sz, snake_sz);
+        console.log(snake.length);
+        snake.shift();
+    }
 }
 
-function chk_meet_sth(x, y)
+function chk_meet_body(x, y)
 {
     snake_len = snake.length
     for (let i = 0; i < snake_len; i++)
@@ -175,16 +183,14 @@ function chk_meet_sth(x, y)
     }
     return false;
 }
-
-function chk_eat_food()
+function chk_eat_food(x, y)
 {
-    snake_len = snake.length
-    for (let i = 0; i < snake_len; i++)
+    snake_len = snake.length;
+    if ((snake[snake_len-1].x == x) && (snake[snake_len-1].y == y))
     {
-        if ((snake[i].x == food_x) && (snake[i].y == food_y))
-        {
-            return true;
-        }
+        score += 30;
+        score_area.innerHTML = "Score: " + score;
+        return true;
     }
     return false;
 }
@@ -192,39 +198,23 @@ function chk_eat_food()
 function draw_snake()
 {
     ctx.moveTo(30, 15);
-    /*
-    for (let i = 0; i<snake_len; i++)
-    {
-        snake[i] = {x: snake_r + i*snake_sz, y:15};
-        ctx.fillStyle = 'rgb(' + Math.floor(255-50.5*(i+1)) + ',' + Math.floor(255-20.5*(i+1)) + ',220)';
-        ctx.arc(snake[i].x, snake[i].y, snake_r, 0, 2 * Math.PI, false);
-        ctx.moveTo(snake[i].x + 45, snake[i].y);
-    }
-    */
+    ctx.fillStyle = c_head;
     //ctx.fillStyle = c_body;
-        ctx.fillStyle = c_head;
     for (let i = 0; i<snake_len; i++)
     {
         snake[i] = {x: i * snake_sz, y:0};
-        //snake.push({x: i*snake_sz, y: 0});
-
         //if (i == (snake_len-1))
         //{
         //    ctx.fillStyle = c_head;
         //}
-
-        //ctx.fillStyle = 'rgb(' + Math.floor(255-50.5*(i+1)) + ',' + Math.floor(255-20.5*(i+1)) + ',220)';
         ctx.fillRect(snake[i].x, snake[i].y, snake_sz, snake_sz);
     }
-
     ctx.stroke();
 }
 
 
 function add_food()
 {
-    ctx.fillStyle ="blue";
-
     food_x = Math.floor(Math.random()*20) * 30;
     food_y = Math.floor(Math.random()*12) * 30;
     for (let i = 0; i<snake_len; i++)
@@ -234,13 +224,18 @@ function add_food()
             add_food();
         }
     }
-    ctx.fillRect(food_x, food_y, snake_sz, snake_sz);
 
+    ctx.fillStyle ="blue";
+    ctx.fillRect(food_x, food_y, snake_sz, snake_sz);
+    ctx.fillStyle = c_head;
 }
 
 window.onload = function()
 {
     draw_snake();
     add_food();
+    ctx.fillStyle = c_head;
+    time_id = setInterval(auto_move, 700);
 }
+
 
